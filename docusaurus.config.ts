a591da8d760d0cb5c7555d7f75d5108d202154ab
Nook <nookittipon@gmail.com>
2024-08-17 23:19:24 +0700

    try config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://snookpy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/react-workshop-docs-2024/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'snookpy', // Usually your GitHub org/user name.
+  projectName: 'react-workshop-docs-2024', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
